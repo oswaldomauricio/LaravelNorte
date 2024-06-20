869f50c5a98dee31e3ae4227ae92db23316d038e
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2024-06-20 17:20:15 -0400

    feat: add home page

---
+++ public/js/navbar.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuButton = document.getElementById('menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    menuButton.addEventListener('click', () => {
+        mobileMenu.classList.toggle('hidden');
+    });
+});
